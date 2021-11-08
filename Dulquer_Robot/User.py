8ef1f71f010b0428a5a7b0d3984153b8cc6d3a5e
mikeyTG <88639357+mikeyTG@users.noreply.github.com>
2021-11-08 11:09:06 +0530

    Update User.py

--- Dulquer_Robot/User.py
+++ Dulquer_Robot/User.py
@@ -3,8 +3,8 @@
 # (c) @AlbertEinsteinTG
 # (e) @Muhammed_RK, @Mo_Tech_YT , @Mo_Tech_Group, @MT_Botz
 # Copyright permission under MIT License
-# All rights reserved by PR0FESS0R-99
-# License -> https://github.com/PR0FESS0R-99/DonLee_Robot/blob/main/LICENSE
+# All rights reserved by mikeyTG
+# License -> https://github.com/mikeyTG/Dulquer_Robot/blob/main/LICENSE
 
 from pyrogram import Client, __version__
 
