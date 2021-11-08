2400235101327a83070476697060250aeba2a3aa
mikeyTG <88639357+mikeyTG@users.noreply.github.com>
2021-11-08 11:12:23 +0530

    Update __main__.py

--- Dulquer_Robot/__main__.py
+++ Dulquer_Robot/__main__.py
@@ -3,7 +3,7 @@
 # (c) @AlbertEinsteinTG
 # (e) @Muhammed_RK, @Mo_Tech_YT , @Mo_Tech_Group, @MT_Botz
 # Copyright permission under MIT License
-# All rights reserved by PR0FESS0R-99
+# All rights reserved by mikeyTG
 # License -> https://github.com/mikeyTG/Dulquer_Robot/blob/main/LICENSE
 
 from .dulquer_robot import Dulquer_Robot
